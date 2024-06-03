66c56ecae9711bb764e73144c6abe8aabb0379ab
muhammedfathy <muhammad_fathui@outlook.com>
2024-06-03 11:49:18 +0300

    add lat,lat column guesser

--- pages/Data manipulation.py
+++ pages/Data manipulation.py
@@ -17,8 +17,16 @@ if uploaded_file is not None:
             data_frame = pd.read_csv(uploaded_file)
         else:
             data_frame = pd.read_excel(uploaded_file)
-        lat = st.selectbox("Choose latitude:", data_frame.columns)
-        lng = st.selectbox("Choose longitude:", data_frame.columns)
+        col1, col2 = st.columns(2)
+        with col1:
+            column_guess = data_frame.columns.str.contains('lat', case=False)
+            latitude_guess = data_frame.columns[column_guess].tolist()[0]
+            lat = st.selectbox("Choose latitude column:", data_frame.columns, index=data_frame.columns.get_loc(latitude_guess))
+        with col2:
+            column_guess = data_frame.columns.str.contains('lng|long', case=False)
+            longitude_guess = data_frame.columns[column_guess].tolist()[0]
+            print(f'***************************{longitude_guess}')
+            lng = st.selectbox("Choose longitude column:", data_frame.columns, index=data_frame.columns.get_loc(longitude_guess))
         data_frame = gpd.GeoDataFrame(
             data_frame,
             geometry=gpd.points_from_xy(data_frame[lng], data_frame[lat]),
@@ -26,28 +34,28 @@ if uploaded_file is not None:
     else:
         data_frame = gpd.read_file(uploaded_file)
 
+    st.write(data_frame)
+    st.map(data_frame, latitude=lat, longitude=lng)
 # input_name = st.text_input("Type you name:", key="name")
 # data_frame = pd.read_csv(r"data/raptor_nest.csv").dropna(
 #     subset=["lat_y_dd", "long_x_dd"]
 # )
-    options = ["Select column"]
-    unique_values = ["Select value"]
-    for column in data_frame.columns:
-        options.append(column)
-    input_column = st.selectbox("Choose column:", options)
-    if input_column != options[0]:
-        for value in data_frame[input_column].unique():
-            unique_values.append(value)
-        if data_frame[input_column].dtype == "O":
-            uvalue = st.selectbox("Select value:", unique_values, index=0)
-        else:
-            uvalue = st.slider(
-                "Select a range of values",
-                data_frame[input_column].min(),
-                data_frame[input_column].max(),
-                (data_frame[input_column].min(), data_frame[input_column].max()),
-            )
-        if uvalue != unique_values[0]:
-            data_frame = data_frame[data_frame[input_column] == uvalue]
-            st.write(data_frame)
-            st.map(data_frame)
+    # options = ["Select column"]
+    # unique_values = ["Select value"]
+    # for column in data_frame.columns:
+    #     options.append(column)
+    # input_column = st.selectbox("Choose column:", options)
+    # if input_column != options[0]:
+    #     for value in data_frame[input_column].unique():
+    #         unique_values.append(value)
+    #     if data_frame[input_column].dtype == "O":
+    #         uvalue = st.selectbox("Select value:", unique_values, index=0)
+    #     else:
+    #         uvalue = st.slider(
+    #             "Select a range of values",
+    #             data_frame[input_column].min(),
+    #             data_frame[input_column].max(),
+    #             (data_frame[input_column].min(), data_frame[input_column].max()),
+    #         )
+    #     if uvalue != unique_values[0]:
+    #         data_frame = data_frame[data_frame[input_column] == uvalue]
