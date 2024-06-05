6159f313bf383d15ad6e5f95b4cee35349b22813
Hossam Nasr Mohamed <h.nasr@wakeb.tech>
2024-06-05 10:36:18 +0300

    add content to home page

--- Home.py
+++ Home.py
@@ -10,4 +10,15 @@ st.set_page_config(
     layout="wide",
 )
 st.sidebar.markdown("# Main page")
-st.markdown("# Geospatial Applications")
+st.sidebar.info("GitHub repository: https://github.com/hossamhassan77/streamlit-geospatial-tools")
+st.sidebar.markdown("# Contact")
+st.sidebar.info(
+    """
+    - LinkedIn: [Hossam Nasr](https://www.linkedin.com/in/hossamnasr7)
+    - Resume: [GIS Developer](https://drive.google.com/file/d/1LDXEsbY_4l67LlP4Zqw0VWSyb0YmKzmO/view?usp=sharing)
+    """
+)
+st.markdown("## 🚀 Unveiling My Python-Powered Geospatial Playground!")
+st.write("""Excited to share my latest project: a web-based platform for geospatial analysis and visualization, built entirely with Python!
+This isn't a commercial venture, but rather a showcase of the skills I've been honing. It's a playground where I can explore various geospatial functionalities and experiment with different analysis and visualization techniques.
+Think of it as a dynamic portfolio, demonstrating the capabilities of Python in the realm of geospatial data.""")
